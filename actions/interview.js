b6e50b12a79f0b50c9f29116f9b6351d54555094
Devpatel32004 <devp222004@gmail.com>
2025-04-14 18:02:09 +0530

    fix a bug

--- actions/interview.js
+++ actions/interview.js
@@ -22,13 +22,13 @@ export async function generateQuiz() {
   if (!user) throw new Error("User not found");
 
   const prompt = `
-    Generate 5 technical interview questions for a ${
+    Generate 10 technical interview questions for a ${
       user.industry
     } professional${
     user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
   }.
     
-    Each question should be multiple choice with 4 options.
+    Each question should be multiple choice with 4 options.try to generate different types of question like easy, medium and hard level
     
     Return the response in this JSON format only, no additional text:
     {
